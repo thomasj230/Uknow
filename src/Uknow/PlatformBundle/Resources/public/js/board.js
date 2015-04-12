e324aa7af5fad32d591123aeeaaeafb00841fcf1
Thomas <thomasj230@gmail.com>
2015-04-12 22:37:58 +0200

    mise à jour des couleurs

--- src/Uknow/PlatformBundle/Resources/public/js/board.js
+++ src/Uknow/PlatformBundle/Resources/public/js/board.js
@@ -3,13 +3,15 @@
  */
 
 $(function(){
+/*
     $('.board').mouseenter(function(){
         $('.board').animate({
             width: '500px'
         }, 'normal');
     }).mouseleave(function(){
         $('.board').animate({
-            width: '250px'
+            width: '200px'
         }, 'fast');
     });
+    */
 });
