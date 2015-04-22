3116d6ee027596e33bd8149ed34fa075cad9af94
Thomas <thomasj230@gmail.com>
2015-04-22 12:46:45 +0200

    Ajout du dropdown pour l'utilisateur

--- src/Uknow/ReadingBundle/Controller/ReadingController.php
+++ src/Uknow/ReadingBundle/Controller/ReadingController.php
@@ -6,7 +6,7 @@
  * Time: 21:26
  */
 
-namespace Uknow\PlatformBundle\Controller;
+namespace Uknow\ReadingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,16 +15,16 @@ class ReadingController extends Controller{
 
     public function lectureAction($id, Request $request){
 
-        return $this->render('UknowPlatformBundle::reading.html.twig');
+        return $this->render('UknowReadingBundle::reading.html.twig');
     }
 
     public function navAction()
     {
-        return $this->render('UknowPlatformBundle:reading/header:nav.html.twig');
+        return $this->render('UknowReadingBundle:header:nav.html.twig');
     }
 
     public function profilAction()
     {
-        return $this->render('UknowPlatformBundle:reading/header:profil.html.twig');
+        return $this->render('UknowReadingBundle:header:profil.html.twig');
     }
 }
