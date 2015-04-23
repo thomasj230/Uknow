48674efefb6b7e6f51b5ee7592a974fb4c2e6099
Thomas <thomasj230@gmail.com>
2015-04-23 13:22:13 +0200

    Ajout du carousel pour l'affichage des cours

--- src/Uknow/ReadingBundle/Resources/public/js/onglets.js
+++ src/Uknow/ReadingBundle/Resources/public/js/onglets.js
@@ -3,10 +3,24 @@
  */
 
 $(function(){
-   $('[data-toggle="tooltip"]').tooltip({delay: { show: 400, hide: 200 }});
+   $('[data-toggle="tooltip"]').tooltip({delay: { show: 200, hide: 200 }});
    $(document).ready(function(){
-       if ($('.reading-onglet-texte').text().length >= 15){
-           $('.reading-onglet-texte').html($('.reading-onglet-texte').text().substring(0, 15) + '...');
+       var i = 0;
+       while($('#reading-onglet-texte-' + i).text().length != 0){
+           if ($('#reading-onglet-texte-' + i).text().length >= 15){
+               $('#reading-onglet-texte-' + i).html($('#reading-onglet-texte-' + i).text().substring(0, 15) + '...');
+           }
+           i = i + 1;
        }
    })
 });
+
+function carouselLeft(){
+    $('.carousel').carousel('prev').carousel('pause');
+}
+
+function carouselRight(){
+    $('.carousel').carousel('next').carousel('pause');
+
+}
+
